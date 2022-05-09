e4902903c5ebaceafb294c9e34a91cf6bdde2d2e
Souranil Sen <souranil@unity3d.com>
2022-05-08 20:26:47 -0700

    Updating dae script

--- scripts/obj2dae.sh
+++ scripts/obj2dae.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-for d in ./data/FigureSkater/refine/*_refine.obj; do
+DATA_PATH=$1
+mkdir -p $DATA_PATH/dae
+
+for d in $DATA_PATH/*_refine.obj; do
     # echo $d
     filename=$(basename $d)
     echo $filename
-    meshlabserver -i $d -o data/FigureSkater/refine/dae/$filename.dae -m vc vn
+    meshlabserver -i $d -o $$DATA_PATH/dae/$filename.dae -m vc vn
 done
